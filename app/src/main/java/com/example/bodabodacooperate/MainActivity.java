c4322cf26311de2c7f741c6627dd9706a46f379a
GeorgeMaduka <georgemaduka92@gmail.com>
2024-05-30 13:49:57 +0300

    The initialization of bodaboda cooporate mobile application

--- app/src/main/java/com/example/bodabodacooperate/MainActivity.java
+++ app/src/main/java/com/example/bodabodacooperate/MainActivity.java
@@ -3,14 +3,51 @@ package com.example.bodabodacooperate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itm = item.getItemId();
+
+        if (itm == R.id.menu_about){
+            Intent menuabout = new Intent(getApplicationContext(), About.class);
+            startActivity(menuabout);
+        }
+
+        if (itm == R.id.menu_about){
+            Intent menucontact = new Intent(getApplicationContext(), Contact.class);
+            startActivity(menucontact);
+        }
+
+        if (itm == R.id.menu_about){
+            Intent menusettings = new Intent(getApplicationContext(), Settings.class);
+            startActivity(menusettings);
+        }
+
+        else {
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
